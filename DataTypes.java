ad298fab6d21e135d346e174a76e4600be0ae7ff
Zach <zachary.brown@vtsd.com>
2022-09-09 12:27:44 -0400

    Add: actual code

---
+++ DataTypes.java
@@ -0,0 +1,9 @@
+//This is a comment!
+/*This is a
+longer comment
+ */
+public class DataTypes {
+    int name = 27;
+    double othername = 3.14;
+    boolean isthisaname = true;
+}
--- Program1.java
+++ Program1.java
@@ -1,3 +1,9 @@
 public class Program1 {
+    public static void main(String args[]){
+
+        System.out.print("hello");
+
+    }
+
+
 }
-hello
