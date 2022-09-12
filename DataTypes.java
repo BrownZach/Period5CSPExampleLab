69d7716edc32614567dfd7f872a0c85c976c7266
Zach <zachary.brown@vtsd.com>
2022-09-12 11:40:56 -0400

    Add: actual code

--- DataTypes.java
+++ DataTypes.java
@@ -1,9 +1,15 @@
-//This is a comment!
+//This is a comment
 /*This is a
 longer comment
  */
 public class DataTypes {
-    int name = 27;
-    double othername = 3.14;
-    boolean isthisaname = true;
+    public static void main(String args[]){
+        int name = 27;
+        double othername = 3.14;
+        boolean isthisaname = true;
+        System.out.println(name);
+        int no1 = 3;
+        int n2 = 7;
+        System.out.print(no1 + n2 / no1);
+}
 }
